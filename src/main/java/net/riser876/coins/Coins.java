d680224e0b59d2b75249a31249cffcc30aa9c5f0
GMalvestiti <gustavo.smalvestiti@gmail.com>
2026-03-25 21:43:19 -0300

    [feat] update fabric 26.1

--- src/main/java/net/riser876/coins/Coins.java
+++ src/main/java/net/riser876/coins/Coins.java
@@ -2,17 +2,21 @@ package net.riser876.coins;
 
 import net.fabricmc.api.ModInitializer;
 import net.riser876.coins.registry.CoinsItem;
-import net.riser876.coins.util.CoinsGlobals;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Coins implements ModInitializer {
+
+    public static final String MOD_ID = "coins";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
         try {
             CoinsItem.init();
-            CoinsGlobals.LOGGER.info("[Coins] Mod loaded.");
+            Coins.LOGGER.info("[Coins] Mod loaded.");
         } catch (Exception e) {
-            CoinsGlobals.LOGGER.error("[Coins] Failed to load.", e);
+            Coins.LOGGER.error("[Coins] Failed to load.", e);
         }
     }
 }
--- src/main/java/net/riser876/coins/registry/CoinsTag.java
+++ src/main/java/net/riser876/coins/registry/CoinsTag.java
@@ -1,15 +1,19 @@
 package net.riser876.coins.registry;
 
 import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
-import net.riser876.coins.util.CoinsGlobals;
+import net.riser876.coins.Coins;
 
 public class CoinsTag {
 
-    public static final TagKey<Item> COINS = TagKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(CoinsGlobals.MOD_ID, CoinsGlobals.MOD_ID));
-    public static final TagKey<Item> GOLD_COIN = TagKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(CoinsGlobals.MOD_ID, CoinId.GOLD_COIN.getId()));
-    public static final TagKey<Item> IRON_COIN = TagKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(CoinsGlobals.MOD_ID, CoinId.IRON_COIN.getId()));
-    public static final TagKey<Item> COPPER_COIN = TagKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(CoinsGlobals.MOD_ID, CoinId.COPPER_COIN.getId()));
+    public static final TagKey<Item> COINS = create(Coins.MOD_ID);
+    public static final TagKey<Item> GOLD_COIN = create(CoinId.GOLD_COIN.getId());
+    public static final TagKey<Item> IRON_COIN = create(CoinId.IRON_COIN.getId());
+    public static final TagKey<Item> COPPER_COIN = create(CoinId.COPPER_COIN.getId());
+
+    private static TagKey<Item> create(String path) {
+        return TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(Coins.MOD_ID, path));
+    }
 }
